751bc4ad9de35a8378aad2627f670bfb4c6d7fad
Dominic DiSanto <34800920+domdisanto@users.noreply.github.com>
2020-04-13 00:24:03 -0400

    Update prediction_app.R
    Added an additional `datatable` object with some of the initial output. The table is editable. I still need to add the "Classification" column for "Seizure/No seizure" prediction status. I also still need to figure out the best way to update the sensitivity and specificity values based on user-selected threshold

--- prediction_app.R
+++ prediction_app.R
@@ -1,23 +1,33 @@
 library(shiny)
-
+library(DT)
 ui <-  fluidPage(
+  
+# title and author information
   titlePanel(h1(strong("Predicting seizure events in moderate-to-severe TBI"))),
   titlePanel(h3("Arvon Clemens, Felix Proessl, Dominic DiSanto")),
   
+# section for input of information/data
 sidebarLayout(
   sidebarPanel(
     
+    
+      # Imaging Inputs (SDH, EDH, CT Fragment)    
            h2(strong("Input Dashboard")),
            h3(strong("Imaging Data")),
            checkboxInput("sdh", label = "Subdural Hematoma?", value = NULL),
            checkboxInput("ctfrag", label = "Intracranial Fragments?", value = NULL),
+           checkboxInput("edh", label = "Epidural Hematoma?", value = NULL),
+
            
-           numericInput("contusions",
-                        label = h3(strong("Number of present contusions in CT imaging")),
-                        value = NULL),
-           checkboxInput("contusions_greater4", label = ">4", value = NULL),
+      # CT Dropdown
+           selectInput("contusion_num", h3(strong("Number of Contusions:")),
+                       c("1" = 1,
+                         "2" = 2,
+                         "3" = 3,
+                         "4+" = 4)),
            
            
+     # Hospitalization/Injury Data (injury severity [PTA, TTFC, GCS], Alcohol, Acute Seizures, Craniotomy/Craniectomy)
            h3(strong("Injury/Hospitalization Information")),
            checkboxInput("pta", label = "Post-traumatic amnesia > 24h?", value = NULL),
            checkboxInput("gcs", label = "GCS < 8?", value = NULL),
@@ -25,30 +35,42 @@ sidebarLayout(
            
            checkboxInput("etoh", label = "Alcohol present at time of hospitalization?", value = NULL),
            checkboxInput("acute_sz", label = "Acute seizures?", value = NULL),
+           checkboxInput("craniotomy", label="Craniotomy?", value=NULL),
+           checkboxInput("craniectomy", label="Craniectomy?", value=NULL),
            
-           
+     # Pre-injury medical history information (Incarceration, Psych Hospitalization, Neurodegenerative Disease)
            h3(strong("Pre-Injury Information")),
            checkboxInput("incarcerate", label = "Pre-Injury history of incarcaration?", value = NULL),
            checkboxInput("psychhosp", label = "Pre-Injury psych hospitalization/institutionalization?", value = NULL),
-           checkboxInput("neurodegen", label = "History of neurodegenerative disease?", value = NULL))
-    
-  ,
+           checkboxInput("neurodegen", label = "History of neurodegenerative disease?", value = NULL)),
+
+# mainPanel: output section for tabular and plotted results
   mainPanel(
-    textOutput("selected_inputs"), 
     textOutput("placeholder"),
-    plotOutput("dummy_plot"))  
+    dataTableOutput("predicted_prob"))  
     
     ))
 
 
 server= function(input, output) { 
-  output$selected_inputs <- renderText({
-    paste("You've selected a patient with", input$contusions, "contusion.")
+  
+  output$predicted_prob <- renderDataTable({ 
+  datatable(data.frame(`Predicted Probability` = 1 / (1 + exp(-(input$incarcerate*0.555 + input$acute_sz*1.352 + input$craniectomy*1.197 + input$ctfrag*0.748 +
+                     input$etoh*0.484 + input$sdh*0.465 + input$psychhosp*.434 + input$craniotomy*0.399 +
+                     input$neurodegen*0.389 + input$edh*0.313)
+                     )), 
+             Threshold=0.1546,
+             Sensitivity=0.75456,
+             Specificity=0.6008), editable = T, rownames = F, options = list(lengthChange=FALSE, 
+                                                                             sDom  = '<"top">lrt<"bottom">ip'))
   })
+  
+  
   output$placeholder <- renderText({
-  "Placeholder text. We will have other tables and output here"
+    "Placeholder text. We will have other tables and output here"
   })
-  }
+
+}
 
 
 shinyApp(ui, server)
